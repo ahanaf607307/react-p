682285787ce337e80288af5bc3891bd170695163
Ahanaf Mubasshir <123632951+ahanaf607307@users.noreply.github.com>
2024-10-17 12:02:19 +0600

    updated video 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import './App.css'
 
 function App() {
+  function handleButton1(){
+    alert("hi 1")
+  }
+  function handleButton2(){
+    alert("hi 2")
+  }
   return (
-    <div>aaa</div>
+    <>
+    <button onClick={handleButton1}>Click 1</button>
+    <button onClick={handleButton2}>Click 2</button>
+    </>
   )
 }
 
